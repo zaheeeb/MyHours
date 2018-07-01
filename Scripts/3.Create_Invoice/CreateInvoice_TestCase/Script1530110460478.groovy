658fec1d065d305cf8942a10e6b7e6f69db10264
Zahir <zaheeeb@gmail.com>
2018-07-01 09:15:07 +0300

    comments added to crete invoice test case

--- Scripts/3.Create_Invoice/CreateInvoice_TestCase/Script1530110460478.groovy
+++ Scripts/3.Create_Invoice/CreateInvoice_TestCase/Script1530110460478.groovy
@@ -20,23 +20,32 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+'Click on Manage menu'
 WebUI.click(findTestObject('3.CreateInvoice_Object_Repository/a_Manage'))
 
+'Click on Invoivce sub menu'
 WebUI.click(findTestObject('3.CreateInvoice_Object_Repository/a_Invoices'))
 
+'Click on Create invoice link'
 WebUI.click(findTestObject('3.CreateInvoice_Object_Repository/a_Create New Invoice'))
 
 WebUI.click(findTestObject('3.CreateInvoice_Object_Repository/input_ng-pristine ng-untouched'))
 
+'check box is checked'
 WebUI.click(findTestObject('3.CreateInvoice_Object_Repository/button_Invoice selected'))
 
+'Tax name entry'
 WebUI.setText(findTestObject('3.CreateInvoice_Object_Repository/input_tax-name'), 'GST')
 
+'tax percentage'
 WebUI.setText(findTestObject('3.CreateInvoice_Object_Repository/input_tax-value'), '14')
 
+'text on invoice is entered'
 WebUI.setText(findTestObject('3.CreateInvoice_Object_Repository/textarea_invoice-text'), 'invoice text')
 
+'hidden note is added'
 WebUI.setText(findTestObject('3.CreateInvoice_Object_Repository/textarea_invoice-note'), 'note not show on invoice.')
 
+'create button is clicked'
 WebUI.click(findTestObject('3.CreateInvoice_Object_Repository/button_Create'))
 
--- Test Suites/MyHours_TestSuite.ts
+++ Test Suites/MyHours_TestSuite.ts
@@ -4,7 +4,7 @@
    <name>MyHours_TestSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-06-30T07:17:48</lastRun>
+   <lastRun>2018-07-01T09:09:23</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
